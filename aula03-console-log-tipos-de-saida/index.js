9245b6a784b2e0ddc0d137d2ce43b07c36a0234f
NicollasRodrigo <nicollas.rodrigoo@gmail.com>
2026-08-04 08:12:03 -0300

    Atualizar conteúdo do repositório para o estado atual

--- aula03-console-log-tipos-de-saida/index.js
+++
@@ -1,5 +0,0 @@
-console.log("Nicollas'Rodrigo'"); // string = Texto
-console.log('Nicollas "Rodrigo"'); // Number = Número
-console.log(`'Nicollas' "Rodrigo"`);
-
-console.log(35, 15.85, "Nicollas Rodrigo De Novo");
